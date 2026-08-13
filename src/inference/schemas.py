2771693b40a8f7522159cabb77896b13016a7578
gitChara-dot <bautistafajardo2006@gmail.com>
2026-08-12 23:24:05 -0300

    feat(api): add 'iterations' field to the prediction request.

--- src/inference/schemas.py
+++ src/inference/schemas.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel, Field
 from typing import Any
+
 class PredictionRequest(BaseModel):
     home_team : str = Field(..., description="Home Team", examples=["Argentina"])
     away_team : str = Field(..., description="Away Team", examples=["France"])
     neutral : bool = Field(default=True, description="True if the match is played on even ground.")
+    iterations : int = Field(default=1, description="Enter the number of possible outcomes. For each iteration, result is less likely. Max 6.", examples=["4"])
 
 class PredictionResponse(BaseModel):
     results : dict[str, Any]
