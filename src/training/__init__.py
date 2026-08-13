53486b0e856a1b12ea7209915d243731adff67ab
gitChara-dot <bautistafajardo2006@gmail.com>
2026-08-13 03:39:14 -0300

    chore(pipeline): add new method to init.

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,8 +1,9 @@
-from .train_pipeline import ModelTrainer, train_models, save_models, run_training_pipeline
+from .train_pipeline import ModelTrainer, train_models, save_models, save_artifacts, run_training_pipeline
 
 __all__ = [
     'ModelTrainer',
     'train_models',
     'save_models',
+    'save_artifacts',
     'run_training_pipeline'
 ]
