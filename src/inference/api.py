c01c980f43a9645ac821ee5a2bc9da16107ce1dc
gitChara-dot <bautistafajardo2006@gmail.com>
2026-08-12 23:24:41 -0300

    feat(api): add middleware to the api app.

--- src/inference/api.py
+++ src/inference/api.py
@@ -1,26 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import joblib
 import os
+import pandas as pd
 
-app = FastAPI(title="WhereItLands Predictor", description="API de Inferencia de Fútbol")
 
 # Variables globales para los modelos
 home_stack = None
 away_stack = None
+elo_system = None
+team_history = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     
     print("Iniciando servidor. Cargando modelos.")
     load_models()
+    load_artifacts()
     try:
         yield
     finally:
         print("Deteniendo servidor.")
-        global home_stack, away_stack
+        global home_stack, away_stack, team_history, elo_system
         home_stack = None
         away_stack = None
+        team_history = None
+        elo_system = None
 
 def load_models():
     """Carga los modelos entrenados desde la bóveda (artifacts) al iniciar la API."""
@@ -37,6 +43,32 @@ def load_models():
         print("Modelos cargados exitosamente.")
     else:
         print("Advertencia: No se encontraron los modelos en artifacts/. Entrena el modelo primero.")
+
+def load_artifacts():
+    """Carga el dataframe historico de los partidos por equipo y el sistema de ELO actualizado al iniciar la API."""
+    global elo_system, team_history
+
+    artifacts_dir = "artifacts"
+    
+    elo_path = os.path.join(artifacts_dir, "elo_system.joblib")
+    dataframe_path = os.path.join(artifacts_dir, "team_history_dataframe.parquet")
+
+    if(os.path.exists(elo_path) and os.path.exists(dataframe_path)):
+        elo_system = joblib.load(elo_path)
+        team_history = pd.read_parquet(dataframe_path)
+        print("Sistema y dataframe cargados correctamente.")
+    else:
+        print("Advertencia: No se encontró el sistema de elo o el registro historico de partidos.")
+        
+app = FastAPI(title="WhereItLands Predictor", description="API de Inferencia de Fútbol", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def root():
